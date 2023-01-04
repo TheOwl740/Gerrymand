e8140420c3b9101b6e5c29d631130978e5ff91e0
Henry Shaw <103895027+TheOwl740@users.noreply.github.com>
2023-01-04 02:05:45 +0000

    tokens and layout

--- script.js
+++ script.js
@@ -1,14 +1,68 @@
-var grid = new Image()
-grid.src = "grid.png";
-grid = new ImageRenderer(grid, 1, 0, 0, (e.data.h / 8) * 5, (e.data.h / 8) * 7, false, false, true, false);
-var logo = new Image()
-logo.src = "logo.png";
-logo = new ImageRenderer(grid, 1, 0, 0, (e.data.h / 8) * 5, (e.data.h / 8), false, false, true, false);
+const images = {
+	grid: new Image(),
+	logo: new Image(),
+	bottomLine: new Image()
+};
+images.grid.src = "grid.png";
+images.grid = new ImageRenderer(images.grid, 1, 0, 0, (e.data.h / 10) * 5, (e.data.h / 10) * 7, false, false, true, false);
+images.logo.src = "logo.png";
+images.logo = new ImageRenderer(images.logo, 1, 0, 0, (e.data.h / 10) * 5, (e.data.h / 20), false, false, true, false);
+images.bottomLine.src = "bottomLine.png";
+images.bottomLine = new ImageRenderer(images.bottomLine, 1, 0, 0, (e.data.h / 10) * 5, (e.data.h / 10), false, false, true, false);
+
+const gridMatrix = [
+	[
+
+	],
+	[
+		
+	],
+	[
+		
+	],
+	[
+		
+	],
+	[
+		
+	]
+]
+for(i = 0; i < 5; i++) {
+	gridMatrix[i].push({
+		seat: null,
+		zone: null
+	},
+	{
+		seat: null,
+		zone: null
+	},
+	{
+		seat: null,
+		zone: null
+	},
+	{
+		seat: null,
+		zone: null
+	},
+	{
+		seat: null,
+		zone: null
+	},
+	{
+		seat: null,
+		zone: null
+	},
+	{
+		seat: null,
+		zone: null
+	});
+}
 
 const timer = setInterval(update, 10);
 
 function update() {
 	e.methods.clearCanvas(new FillRenderer("#DDFFEE", null, 1, 0));
-	e.methods.renderImage(new Transform(e.data.w / 2, (e.data.h / -2) + (e.data.h / -16), 0), grid);
-	e.methods.renderImage(new Transform(e.data.w / 2, (e.data.h / -2) + (e.data.h / -16), 0), grid);
+	e.methods.renderImage(new Transform(e.data.w / 2, (e.data.h / -20), 0), images.logo);
+	e.methods.renderImage(new Transform(e.data.w / 2, (e.data.h / -2), 0), images.grid);
+	e.methods.renderImage(new Transform(e.data.w / 2, (e.data.h / 15) - e.data.h, 0), images.bottomLine);
 }
